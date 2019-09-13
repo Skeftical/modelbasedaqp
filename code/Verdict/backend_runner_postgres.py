bb97ef551b225b27a6fba2df574639035ea1142c
unknown <fotissavva@hotmail.com>
2019-09-13 11:49:38 +0100

    adding answer as dataframe

--- code/Verdict/backend_runner_postgres.py
+++ code/Verdict/backend_runner_postgres.py
@@ -18,26 +18,27 @@ import pandas as pd
 #    logger.addHandler(handler)
 #
 # print(args.source)
+if not os.path.exists('../../output/backend-postgres-actual'):
+        logger.info('creating directory Accuracy')
+        os.makedirs('../../ooutput/backend-postgres-actual')
 
 if __name__=='__main__':
     print("main executing")
     directory = os.fsencode('temp')
     conn = psycopg2.connect(host='127.0.0.1',port=5433,dbname='tpch1g',user='analyst',password='analyst')
     cur = conn.cursor()
-    cur.execute("SELECT COUNT(*) FROM lineitem;")
-    print(cur.fetchall())
     query_answers_dic = {}
-    query_answers_dic['query'] = []
-    query_answers_dic['result'] = []
     query_answers_dic['time'] = []
     for f in os.listdir(directory):
         print(f)
-        print("Query Name : {0}".format(os.fsdecode(f).split('.')[0]))
+        query_name = os.fsdecode(f).split('.')[0]
+        print("Query Name : {0}".format(query_name))
         with open(os.path.join(directory,f),"r") as sql_query_file:
             sql_query = sql_query_file.read()
             print(sql_query)
             cur.execute(sql_query)
             res = cur.fetchall()
-            print(pd.DataFrame(res))
+            res_df = pd.DataFrame(res)
+            res_df.to_pickle('../../output/backend-postgres-actual/{}.pkl'.format(query_name))
     cur.close()
     conn.close()
