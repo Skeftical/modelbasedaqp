108dbbe1f15d9bf5e411a0348593972ef0e0db2e
unknown <fotissavva@hotmail.com>
2019-09-16 10:17:41 +0100

    instacart query vectorization

--- code/Data_Generation/instacart_query_vectorization.py
+++ code/Data_Generation/instacart_query_vectorization.py
@@ -3,7 +3,22 @@ import sys
 os.chdir('../../')
 import pickle
 import psycopg2
+import pandas as pd
 
 queries = []
 with open('input/instacart_queries/queries.pkl','rb') as f:
     queries = pickle.load(f)
+
+conn = psycopg2.connect(host='127.0.0.1',port=5433,dbname='instacart',user='analyst',password='analyst')
+cur = conn.cursor()
+
+for q in queries:
+    print(q)
+    cur.execute(sql_query)
+    res = cur.fetchall()
+    res_df = pd.DataFrame(res)
+
+    print(res_df)
+    break;
+cur.close()
+conn.close()
