fd68354a749d001daa4f3780fb1813b2984b57c5
unknown <fotissavva@hotmail.com>
2019-09-16 11:32:15 +0100

    obtain all attributes for db

--- code/Data_Generation/instacart_query_vectorization.py
+++ code/Data_Generation/instacart_query_vectorization.py
@@ -12,6 +12,10 @@ with open('input/instacart_queries/queries.pkl','rb') as f:
 pr = Parser()
 conn = psycopg2.connect(host='127.0.0.1',port=5433,dbname='instacart',user='analyst',password='analyst')
 cur = conn.cursor()
+#Obtain all attribute columns
+cur.execute("SELECT column_name FROM information_schema.columns WHERE table_schema='public';")
+res = cur.fetchall()
+print(res)
 
 for q in queries:
     print(q)
