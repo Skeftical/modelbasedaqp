f8944180b2ff3a2ddff3347ac6a9ea517e3315a0
unknown <fotissavva@hotmail.com>
2019-09-19 09:47:36 +0100

    edited query template

--- instacart_query_templates/q15.sql
+++ instacart_query_templates/q15.sql
@@ -6,11 +6,11 @@
 -- GROUP BY departments.department_id, department
 -- ORDER BY order_count DESC
 -- LIMIT 5;
-SELECT departments.department_id, department, count(*) as order_count
+SELECT departments.departmentid, department, count(*) as order_count
 FROM order_products, orders, products, departments
 WHERE orders.order_id = order_products.order_id
   AND order_products.product_id = products.product_id
-  AND products.department_id = departments.department_id
-GROUP BY departments.department_id, department
+  AND products.departmentid = departments.departmentid
+GROUP BY departments.departmentid, department
 -- ORDER BY order_count DESC
 -- LIMIT 5;
