bcdc57efde249d2f87889737864d037e8db787d3
Fotis <fotissavva@hotmail.com>
2019-09-20 16:36:16 +0100

    made changes to runner

--- code/Verdict/runner.py
+++ code/Verdict/runner.py
@@ -3,7 +3,8 @@ import argparse
 import logging
 import os
 import time
-#
+import pandas as pd
+
 # parser = argparse.ArgumentParser()
 # parser.add_argument("--verbose", dest='verbosity', help="increase output verbosity",
 #                     action="store_true")
@@ -25,30 +26,34 @@ if not os.path.exists('../../output/verdict/tpch'):
 
 if __name__=='__main__':
     print("main executing")
-    directory = os.fsencode('temp')
+    directory = os.fsencode('/home/fotis/Desktop/tpch_2_17_0/dbgen/tpch_queries_10/')
 
     verdict = pyverdict.postgres('127.0.0.1',5433,dbname='tpch1g',user='analyst',password='analyst')
-    res = verdict.sql("""CREATE SCRAMBLE IF NOT EXISTS public.lineitem_x
-                        FROM public.lineitem SIZE 0.1""")
-    verdict.sql("""CREATE SCRAMBLE IF NOT EXISTS public.orders_x
-                        FROM public.orders SIZE 0.1""")
-    verdict.sql("""CREATE SCRAMBLE IF NOT EXISTS public.partsupp_x
-                        FROM public.partsupp SIZE 0.1""")
-    print(res)
+#    res = verdict.sql("""CREATE SCRAMBLE IF NOT EXISTS public.lineitem_x
+#                        FROM public.lineitem SIZE 0.1""")
+#    verdict.sql("""CREATE SCRAMBLE IF NOT EXISTS public.orders_x
+#                        FROM public.orders SIZE 0.1""")
+#    verdict.sql("""CREATE SCRAMBLE IF NOT EXISTS public.partsupp_x
+ #                       FROM public.partsupp SIZE 0.1""")
+#    print(res)
     query_answers_dic = {}
     query_answers_dic['query_name'] = []
     query_answers_dic['time'] = []
     for f in os.listdir(directory):
         print(f)
+        query_name = os.fsdecode(f).split('.')[0]
         print("Query Name : {0}".format(os.fsdecode(f).split('.')[0]))
         with open(os.path.join(directory,f),"r") as sql_query_file:
             sql_query = sql_query_file.read()
             start = time.time()
-            res_df_v = verdict.sql(sql_query)
+            try:
+                res_df_v = verdict.sql(sql_query)
+            except Exception:
+                print("Query {} not supported".format(query_name))
             end = time.time()-start
             res_df_v.to_pickle('../../output/verdict/tpch/{}.pkl'.format(query_name))
             query_answers_dic['time'].append(end)
             query_answers_dic['query_name'].append(query_name)
     verdict.close()
     qa = pd.DataFrame(query_answers_dic)
-    qa.to_csv('../../output/verdict/query-response-time.csv')
+    qa.to_csv('../../output/verdict/tpch/query-response-time.csv')
