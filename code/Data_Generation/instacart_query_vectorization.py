219ec918cc8c3b637cd9627bb07913803b45e145
unknown <fotissavva@hotmail.com>
2019-09-23 16:31:00 +0100

    setting index appropriately

--- code/Data_Generation/instacart_query_vectorization.py
+++ code/Data_Generation/instacart_query_vectorization.py
@@ -79,7 +79,7 @@ for qname,q in queries:
             print(temp.columns)
 
             print(qdf.loc[i:i+res_df.shape[0]-1,'reordered_lb'])
-            temp = temp.set_index(i,i+res_df.shape[0])
+            temp = temp.set_index(np.arange(i,i+res_df.shape[0]))
             print(temp[proj_list])
             qdf.loc[i:i+res_df.shape[0], proj_list] = temp[proj_list]
         except KeyError:
