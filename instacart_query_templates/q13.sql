4b1e96db9b75728ee0c17783e95d4cb87069059c
unknown <fotissavva@hotmail.com>
2019-09-24 21:58:03 +0100

    edited sql

--- instacart_query_templates/q13.sql
+++ instacart_query_templates/q13.sql
@@ -1,4 +1,4 @@
-select order_hour_of_day, count(*) as c
+select order_hour_of_day, count(*)
 from orders
 group by order_hour_of_day
 order by order_hour_of_day;
--- instacart_query_templates/q14.sql
+++ instacart_query_templates/q14.sql
@@ -6,7 +6,7 @@
 -- GROUP BY product_name
 -- ORDER BY order_count DESC
 -- LIMIT 5;
-SELECT product_name, count(*) as order_count
+SELECT product_name, count(*)
 FROM order_products, orders, products
 WHERE orders.order_id = order_products.order_id
   AND order_products.product_id = products.product_id
