f06bc2d3486be250a23aaea27452084c69fde03b
unknown <fotissavva@hotmail.com>
2019-09-30 16:26:17 +0100

    edited MLAF

--- ml/model.py
+++ ml/model.py
@@ -7,19 +7,18 @@ class MLAF:
     """
 
     def predict_one(self, attr_dict):
-        for k in attr_dict:
-            self.vector_dict[k] = attr_dict[k]
-        query_vector = np.array(list(self.vector_dict.values())).reshape(1,-1)
-        if af!='count':
-            query_vector = xgb.DMatrix(query_vector)
+        array = []
+        for k in self.features:
+            array.append(np.float(attr[k]))
+    #     temp.append(m.predict_one(attr))
+        query_vector = xgb.DMatrix(np.array(array).reshape(1,-1), feature_names=self.features)
         return float(self.estimator.predict(query_vector))
 
     def __init__(self, estimator, rel_error, feature_names, af):
         self.estimator = estimator
         self.AF = af
         self.rel_error = rel_error
-        self.vector_dict = { key : np.nan for key in feature_names}
-        print(self.vector_dict)
+        self.features = feature_names
 
 
 if __name__=="__main__":
