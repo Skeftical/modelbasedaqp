4e1c16aa5cf28099196784c66f2532c6f9e7dadd
Fotis <fotissavva@hotmail.com>
2019-10-04 11:42:41 +0100

    minor fixes to performance queries

--- code/Performance_Storage/performance_queries.py
+++ code/Performance_Storage/performance_queries.py
@@ -9,6 +9,7 @@ import numpy as np
 from ml.model import MLAF
 import xgboost as xgb
 import time
+from sklearn.model_selection import train_test_split
 
 
 
@@ -27,7 +28,7 @@ count_columns = [name for name in qdf[targets].columns if 'count' in name]
 #Generate Dataframes per aggregate function
 count_df = qdf.iloc[qdf['count'].dropna(axis=0).index]
 
-features = [name for name in sum_df.columns if name not in ['sum_add_to_cart_order','avg_add_to_cart_order','count']]
+features = [name for name in count_df.columns if name not in ['sum_add_to_cart_order','avg_add_to_cart_order','count']]
 
 
 target_count = 'count'
@@ -47,15 +48,15 @@ query_results['time'] = []
 for no in no_queries:
 
     X = count_df.loc[:no, features].values
-    y = count_df.loc[:no, label].values
+    y = count_df.loc[:no, target_count].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=1234)
     dtrain = xgb.DMatrix(X_train,y_train, feature_names=features)
     dtest = xgb.DMatrix(X_test, y_test, feature_names=features)
 
-    params = {'max_depth':6, 'eta':0.3, 'objective':custom_relative_error_loss, 'eval_metric':['rmse'],'colsample_bytree':0.75, 'colsample_bylevel':0.75, 'colsample_bynode':0.75, 'reg_alpha':0.3, 'reg_lambda':1}
+    params = {'max_depth':6, 'eta':0.3, 'objective': 'reg:squarederror', 'eval_metric':['rmse'],'colsample_bytree':0.75, 'colsample_bylevel':0.75, 'colsample_bynode':0.75, 'reg_alpha':0.3, 'reg_lambda':1}
     for i in range(10):
         start = time.time()
-        xgb_model = xgb.train(params, dtrain,num_boost_round=1000,early_stopping_rounds=10, feval=f_relative_error, evals=[(dtrain,'train'),(dtest,'test')],
+        xgb_model = xgb.train(params, dtrain,num_boost_round=1000,early_stopping_rounds=10, evals=[(dtrain,'train'),(dtest,'test')],
              verbose_eval=True)
         end = time.time()-start
         query_results['no_queries'].append(no)
