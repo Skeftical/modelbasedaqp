923739df74b297bdcfe8633fb9abef12e9c36266
unknown <fotissavva@hotmail.com>
2019-10-09 21:40:17 +0100

    added caller

--- code/model-serving/caller.py
+++ code/model-serving/caller.py
@@ -1,18 +1,10 @@
-import boto3
 import json
-client = boto3.client('lambda', region_name='eu-west-1', aws_access_key_id='AKIAYZYQQOACW4NY577K',
-    aws_secret_access_key='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',)
+import requests
 
-res = client.invoke(
-    FunctionName='arn:aws:lambda:eu-west-1:605088149509:function:mytestfunction',
-    Payload=json.dumps(
-       {
-        "groups" : ["groups"], 
-        "projections": ["count"],
-        "filters": {"order_dow_lb": 4.0, "order_dow_ub": 4.0}
+event = {}
+event['projections'] = ['count']
+event['groups'] = ['product_name']
+event['filters']= {'order_dow_lb': 4.0, 'order_dow_ub': 4.0}
 
-       }    
-      ) 
-    )
-print(res)
-print(json.loads(res['Payload'].read()))
+r = requests.post("localhost:5000", data=event)
+print(r.status_code, r.reason)
