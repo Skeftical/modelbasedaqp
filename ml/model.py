2b66123ac02e20170d935e85ffec65632d3d18f4
Fotis <fotissavva@hotmail.com>
2019-10-16 21:55:39 +0100

    edited model.py

--- ml/model.py
+++ ml/model.py
@@ -16,14 +16,16 @@ class MLAF:
         for k in self.features:
             array.append(np.float(attr_dict.get(k, np.nan)))
     #     temp.append(m.predict_one(attr))
-        query_vector = pd.Dataframe(array, columns=features)
-        return float(self.estimator.predict(query_vector))
+   #     query_vector = pd.Dataframe(array, columns=features)
+        return float(self.estimator.predict(np.array(array).reshape(1,-1)))
 
     def predict_many(self, attr_dict):
         qv = QueryVectorizer(self.features, SET_OWN=True)
         for a in attr_dict:
             qv.insert(a, attr_dict[a])
-        query_matrix = qv.to_dataframe()
+        query_matrix = qv.to_dataframe().infer_objects()
+        if self.AF=='count':
+             query_matrix['product_name_lb'] = query_matrix['product_name_lb'].astype('category')
         return self.estimator.predict(query_matrix)
 
     def __init__(self, estimator, rel_error, feature_names, af):
