f42297188b7495a93ade3b3726bf137f809fdb63
Fotis <fotissavva@hotmail.com>
2019-10-17 14:05:51 +0100

    experiments for chicago

--- code/Accuracy/accuracy_chicago.py
+++ code/Accuracy/accuracy_chicago.py
@@ -5,20 +5,15 @@ from scipy import stats
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 #Models
-from xgboost import XGBRegressor
-from sklearn.cluster import KMeans
-from sklearn.linear_model import Ridge
+#from xgboost import XGBRegressor
+
+from lightgbm import LGBMRegressor
 #Metrics
 from sklearn import metrics
 import sys
 import os
 os.chdir('../../')
 sys.path.append('.')
-from confs import Config
-from ML_models.GrowingMiniBatchKMeans import GrowingMiniBatchKMeans
-from ML_models.GrowingNetwork import GrowingNetwork
-from ML_models.LocalSupervisedNetwork import LocalSupervisedNetwork, LocalSupervisedOfflineNetwork
-from terminal_outputs import printProgressBar
 
 
 def load_data():
@@ -26,21 +21,13 @@ def load_data():
     global train_df
     global test_df
 
-    train_df = pd.read_csv('input/Crimes_Workload/train_workload_gauss-{}-users-{}.csv'.format(Config.chicago_clusters,Config.chicago_queries), header=0, index_col=0)
-    test_df = pd.read_csv('input/Crimes_Workload/test_workload_gauss-{}-users-{}.csv'.format(Config.chicago_clusters,Config.chicago_queries), header=0, index_col=0)
+    train_df = pd.read_csv('input/Crimes_Workload/train_workload_gauss-5-users-50000.csv', header=0, index_col=0)
+    test_df = pd.read_csv('input/Crimes_Workload/test_workload_gauss-5-users-50000.csv', header=0, index_col=0)
     train_df.reset_index(drop=True, inplace=True)
     test_df.reset_index(drop=True, inplace=True)
-    test_df = test_df.dropna()
-    train_df = train_df.dropna()
-    test_df['count'] = test_df['count'].apply(np.exp)
-    test_df['sum_'] = test_df['sum_'].apply(np.exp)
-    train_df['count'] = train_df['count'].apply(np.exp)
-    train_df['sum_'] = train_df['sum_'].apply(np.exp)
-
-    test_df['count'] = test_df['count'].replace([np.inf, -np.inf], np.nan).dropna()
-    test_df['sum_'] = test_df['sum_'].replace([np.inf, -np.inf], np.nan).dropna()
-    train_df['count'] = train_df['count'].replace([np.inf, -np.inf], np.nan).dropna()
-    train_df['sum_'] = train_df['sum_'].replace([np.inf, -np.inf], np.nan).dropna()
+    
+    test_df = test_df.replace([np.inf,-np.inf], np.nan).dropna()
+    train_df = train_df.replace([np.inf, -np.inf], np.nan).dropna()
     cutoff = train_df.count()[0]
     complete = pd.concat([train_df, test_df], ignore_index=True)
     complete['x_l'] = complete['x']-complete['x_range']
@@ -78,29 +65,34 @@ def assess_on_queries():
                 X_test = X_test[y_test!=0]
                 y_test = y_test[y_test!=0]
             print("Reconfigured shape {}".format(y_test.shape[0]))
-            xgb = XGBRegressor()
-            scaler = StandardScaler()
-            scaler.fit(X_train)  # Don't cheat - fit only on training data
-            X_train = scaler.transform(X_train)
-            X_test = scaler.transform(X_test)  # apply same transformation to test data
 
-            xgb.fit(X_train, y_train)
-            y_pred = xgb.predict(X_test)
+            lgb = LGBMRegressor(n_estimators=500)
+
+            lgb.fit(X_train, y_train)
+#            scaler = StandardScaler()
+#            scaler.fit(X_train)  # Don't cheat - fit only on training data
+#            X_train = scaler.transform(X_train)
+#            X_test = scaler.transform(X_test)  # apply same transformation to test data
+
+            lgb.fit(X_train, y_train)
+            y_pred = lgb.predict(X_test)
+            print(stats.describe(y_pred))
+            print(stats.describe(y_test))
             if s=='SUM':
                 print((y_pred==y_pred[0]).sum())
                 print(((y_test==np.inf) | (y_test==-np.inf) | (y_test==np.nan)).sum())
             try:
-                rel_error_ML_sum = np.median(np.abs(y_pred-y_test)/y_test)
+                rel_error_ML_sum = np.mean(np.abs(y_test-y_pred)/y_test)
                 nrmsd = np.sqrt(metrics.mean_squared_error(y_test, y_pred))/np.std(y_test)
                 rmse = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
                 mae = metrics.median_absolute_error(y_test, y_pred)
                 rel_errs_ml_queries.append([no, s, rmse, mae, nrmsd, rel_error_ML_sum])
             except ValueError:
                 errs+=1
-        print("Errors on {} of queries".format(errs/Config.chicago_queries))
+        print("Errors on {} of queries".format(errs/50000))
     print("Saving Files...")
     eval_df = pd.DataFrame(rel_errs_ml_queries, columns=['queries', 'aggregate','rmse','mae', 'nrmsd', 'rel_error_median'])
-    eval_df.to_csv('output/accuracy/csvs/chicago_assessment_on_queries_{}_queries.csv'.format(Config.chicago_queries))
+    eval_df.to_csv('output/accuracy/csvs/chicago_assessment_on_queries_50000_queries.csv')
 
 
 if __name__=='__main__':
