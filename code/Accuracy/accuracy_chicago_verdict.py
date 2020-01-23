1661834346cc4d4c05b15c2ef85b38778302013a
unknown <fotissavva@hotmail.com>
2020-01-23 15:33:49 +0000

    created complete script for testing on crimes

--- code/Accuracy/accuracy_chicago_verdict.py
+++ code/Accuracy/accuracy_chicago_verdict.py
@@ -48,81 +48,34 @@ if __name__=='__main__':
     print(res)
     #Evaluate on queries
     test_queries = load_data()
-    print(test_queries.head(5))
+    query_answers_dic = {}
+    query_answers_dic['y_count'] = []
+    query_answers_dic['y_hat_count'] = []
+    query_answers_dic['y_sum'] = []
+    query_answers_dic['y_hat_sum'] = []
+    query_answers_dic['y_avg'] = []
+    query_answers_dic['y_hat_avg'] = []
+
     for tup in test_queries.iterrows():
-        print(tup)
         y_count, y_sum, y_avg, x_l, x_h, y_l, y_h = tup[1]
+        print(y_count, y_sum, y_avg, x_l, x_h, y_l, y_h)
+        res = verdict.sql("""
+            SELECT COUNT(*), SUM(arrest), AVG(beat)
+            FROM crimes_x
+            WHERE x_coordinate>={} AND x_coordinate<={}
+            AND y_coordinate>={}   AND y_coordinate<={}
+        """.format(x_l, x_h, y_l, y_h))
+        y_hat_count = float(np.log(res['c2'].values.astype(float)))
+        y_hat_sum = float(np.log(res['s3'].values.astype(float)))
+        y_hat_avg = float(res['a4'].values.astype(float))
+
+        query_answers_dic['y_count'].append(y_count)
+        query_answers_dic['y_hat_count'].append(y_hat_count)
+        query_answers_dic['y_sum'].append(y_sum)
+        query_answers_dic['y_hat_sum'].append(y_hat_sum)
+        query_answers_dic['y_avg'].append(y_avg)
+        query_answers_dic['y_hat_avg'].append(y_hat_avg)
         break;
-    print(y_count, y_sum, y_avg, x_l, x_h, y_l, y_h)
-    res = verdict.sql("""
-        SELECT COUNT(*), SUM(arrest), AVG(beat)
-        FROM crimes_x
-        WHERE x_coordinate>={} AND x_coordinate<={}
-        AND y_coordinate>={}   AND y_coordinate<={}
-    """.format(x_l, x_h, y_l, y_h))
-    print(res)
-
-    y_hat_count = float(np.log(res['c2'].values.astype(float)))
-    y_hat_sum = float(np.log(res['s3'].values.astype(float)))
-    y_hat_avg = float(res['a4'].values.astype(float))
-    print(y_hat_count, y_hat_sum, y_hat_avg)
-
-    # verdict.sql("DROP ALL SCRAMBLE public.lineitem;")
-    # verdict.sql("DROP ALL SCRAMBLE public.orders;")
-    # verdict.sql("DROP ALL SCRAMBLE public.partsupp;")
-    # res = verdict.sql("""CREATE SCRAMBLE IF NOT EXISTS public.lineitem_x
-    #                   FROM public.lineitem SIZE {}""".format(sampling_ratio))
-    # verdict.sql("""CREATE SCRAMBLE IF NOT EXISTS public.orders_x
-    #                   FROM public.orders SIZE {}""".format(sampling_ratio))
-    # verdict.sql("""CREATE SCRAMBLE IF NOT EXISTS public.partsupp_x
-    #                   FROM public.partsupp SIZE {}""".format(sampling_ratio))
-
-
-    # query_answers_dic = {}
-    # query_answers_dic['query_name'] = []
-    # query_answers_dic['time'] = []
-    # query_names = {}
-    # i = 0
-    # regex_lineitem = re.compile(r"lineitem", re.IGNORECASE)
-    # regex_orders = re.compile(r"orders", re.IGNORECASE)
-    # regex_order_partsupp = re.compile(r"partsupp", re.IGNORECASE)
-    # list_of_files = os.listdir(directory)
-    # for f in list_of_files:
-    #     print(f)
-    #     query_name = os.fsdecode(f).split('.')[0].split('-')[0]
-    #     if query_name not in ['1', '3', '4', '5', '6']:
-    #         continue;
-    #     print("Query Name : {0}".format(os.fsdecode(f).split('.')[0]))
-    #     with open(os.path.join(directory,f),"r") as sql_query_file:
-    #         sql_query = sql_query_file.read()
-    #     sql_query = regex_lineitem.sub("lineitem_x",sql_query)
-    #     sql_query = regex_orders.sub("orders_x",sql_query)
-    #     sql_query = regex_order_partsupp.sub("partsupp_x",sql_query)
-    #     print(sql_query)
-    #     start = time.time()
-    #     try:
-    #         res_df_v = verdict.sql(sql_query)
-    #     except Exception:
-    #         print("Query {} not supported".format(query_name))
-    #         i+=1
-    #         continue;
-    #     end = time.time()-start
-    #     print(res_df_v)
-    #     if 'o_orderdate' in res_df_v.columns:
-    #         res_df_v['o_orderdate'] = res_df_v['o_orderdate'].astype(str)
-    #
-    #     res_df_v.to_pickle('../../output/verdict/tpch-{}/{}.pkl'.format(sampling_ratio,i))
-    #
-    #     if query_name not in query_names:
-    #         query_names[query_name] = [i]
-    #     else:
-    #         query_names[query_name].append(i)
-    #     query_answers_dic['time'].append(end)
-    #     query_answers_dic['query_name'].append(query_name)
-    #     i+=1
-    #
-    # verdict.close()
-    # qa = pd.DataFrame(query_answers_dic)
-    # qa.to_csv('../../output/verdict/tpch-{}/query-response-time.csv'.format(sampling_ratio))
-    # with open('../../output/verdict/tpch-{}/query-assoc-names.pkl'.format(sampling_ratio), 'wb') as f:
-    #     pickle.dump(query_names, f)
+    verdict.close()
+    qa = pd.DataFrame(query_answers_dic)
+    qa.to_csv('../../output/verdict/crimes-{}/predictions-answers.csv'.format(sampling_ratio))
